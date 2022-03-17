df2e7f8f5e0249daa70dc9acce399e5e15e7c644
Felipe <felipe.carvalho.bs.contato@gmail.com>
2022-03-17 01:05:05 -0300

    initial commit

---
+++ Sources/CollegeManager/src/test/java/io/github/zodh/college/manager/CollegeManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zodh.college.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
