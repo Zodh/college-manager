6b2f9898b64d3857a222cfa15961c02ad454078f
Felipe <felipe.carvalho.bs.contato@gmail.com>
2022-03-19 23:20:01 -0300

    feat: error mapper

---
+++ Sources/CollegeManager/src/main/java/io/github/zodh/college/manager/mappers/ErrorMapper.java
@@ -0,0 +1,15 @@
+package io.github.zodh.college.manager.mappers;
+
+import io.github.zodh.college.manager.exceptions.FlowException;
+import io.github.zodh.model.ErrorResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(unmappedSourcePolicy = ReportingPolicy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ErrorMapper {
+
+  @Mapping(source = "flowException.requestId", target = "requestId")
+  @Mapping(source = "flowException.errorDescription", target = "errorDescription")
+  ErrorResponse fromFlowExceptionToErrorResponse(FlowException flowException);
+}
