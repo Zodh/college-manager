2118e6c55d6772d8b481d700e9c1f846e9e1d73e
Felipe <felipe.carvalho.bs.contato@gmail.com>
2022-03-20 00:15:04 -0300

    feat: list course

--- Sources/CollegeManager/src/main/java/io/github/zodh/college/manager/services/courses/CourseService.java
+++ Sources/CollegeManager/src/main/java/io/github/zodh/college/manager/services/courses/CourseService.java
@@ -2,9 +2,12 @@ package io.github.zodh.college.manager.services.courses;
 
 import io.github.zodh.model.CreateCourseRequest;
 import io.github.zodh.model.CreateCourseResponse;
+import io.github.zodh.model.ListCourseResponse;
 
 public interface CourseService {
 
   CreateCourseResponse createCourse(String user, CreateCourseRequest createCourseRequest);
+
+  ListCourseResponse listCourses(String user);
 
 }
